ac8f90f966045dff58f01f11107b3e99ef421935
MurimiCaroline <murimicaroline20@gmail.com>
2025-02-07 15:54:33 +0300

    change icons on activity details

--- lib/data/energy_details.dart
+++ lib/data/energy_details.dart
@@ -2,9 +2,9 @@ import 'package:eco_wise/model/energy_model.dart';
 
 class EnergyDetails{
   final energyData = [
-    EnergyModel(icon: "assets/images/icon.png", value: "12%", title: "Lighting Usage"),
-    EnergyModel(icon: "assets/images/icon.png", value: "12%", title: "Lighting Usage"),
-    EnergyModel(icon: "assets/images/icon.png", value: "12%", title: "Lighting Usage"),
-    EnergyModel(icon: "assets/images/icon.png", value: "12%", title: "Lighting Usage"),
+    EnergyModel(icon: "assets/images/ic_launcher.png", value: "12%", title: "Lighting Usage"),
+    EnergyModel(icon: "assets/images/ic_launcher.png", value: "12%", title: "Lighting Usage"),
+    EnergyModel(icon: "assets/images/ic_launcher.png", value: "12%", title: "Lighting Usage"),
+    EnergyModel(icon: "assets/images/ic_launcher.png", value: "12%", title: "Lighting Usage"),
   ];
 }
